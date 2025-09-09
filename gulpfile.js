23a66384b7666c4f3b5e2dc937df2500215f2587
Roman Savitskyi <roman.savitskyi@gmail.com>
2025-09-10 00:52:18 +0300

    fix (gulp)

--- gulpfile.js
+++ gulpfile.js
@@ -273,7 +273,10 @@ gulp.task('test', gulp.series( 'eslint', 'qunit' ))
 
 gulp.task('default', gulp.series(gulp.parallel('js', 'css', 'plugins'), 'test'))
 
-gulp.task('build', gulp.parallel('js', 'css', 'plugins'))
+gulp.task('html', () => gulp.src(['index.html'])
+    .pipe(gulp.dest('./dist')))
+
+gulp.task('build', gulp.parallel('js', 'css', 'plugins', 'html'))
 
 gulp.task('package', gulp.series(async () => {
 
